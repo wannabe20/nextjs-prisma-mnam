90e1f9fb8d484bd9fb126151e38811257aa9e4bf
wannabe20 <prchlik1819@gmail.com>
2024-03-11 07:55:44 +0100

    krasotinky

--- components/TodoForm.tsx
+++ components/TodoForm.tsx
@@ -20,18 +20,18 @@ function TodoForm() {
 
   return (
     <section className='p-6 border-l border-r'>
-      <form onSubmit={handleAddTodo} className='flex flex-row bg-white border'>
+      <form onSubmit={handleAddTodo} className='flex flex-row border'>
         <input
           type='text'
           name='body'
           value={body}
           onChange={(e) => setBody(e.target.value)}
-          className='ml-2 w-full outline-none'
+          className='ml-2 w-full outline-none text-white bg-black'
           placeholder='Uspokoj přítelkyni...'
         />
         <input
           type='submit'
-          className='p-2 ml-auto bg-blue-500 text-white hover:bg-blue-700 cursor-pointer'
+          className='p-2 ml-auto bg-blue-500 hover:bg-blue-700 cursor-pointer'
           value='Add Todo'
         />
       </form>
